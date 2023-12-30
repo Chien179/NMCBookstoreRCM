d278b28917f77fb8033f6ee9c8c0ae5ecb985cc9
Chien179 <chiennmjb@gmail.com>
2023-12-30 15:59:06 +0700

    update rcm logic

--- src/helper/rcm_book.py
+++ src/helper/rcm_book.py
@@ -31,69 +31,65 @@ class RCMBookHelper:
     def content_based_recommender(self, name: str, size: int):
         self.preprocessing()
         name = str(name).replace('"', "")
-        if name in self.df["name"].values:
-            data = self.df["name"].value_counts()
-            rating_counts = pd.DataFrame(data)
-            rare_books = rating_counts[rating_counts.to_numpy() <= 2].index
-            self.common_books = self.df[~self.df["name"].isin(rare_books)]
+        data = self.df["name"].value_counts()
+        rating_counts = pd.DataFrame(data)
+        rare_books = rating_counts[rating_counts.to_numpy() <= 2].index
+        self.common_books = self.df[~self.df["name"].isin(rare_books)]
 
-            if name in rare_books:
-                logger.info("rare book")
-                random = pd.Series(self.common_books["name"].unique()).sample(16).values
-                books = []
-                for book in random:
-                    book_cor = {
-                        "id": self.get_coll_by_name(book, "id"),
-                        "name": self.get_coll_by_name(book, "name"),
-                        "price": self.get_coll_by_name(book, "price"),
-                        "image": self.get_coll_by_name(book, "image"),
-                        "description": self.get_coll_by_name(book, "description"),
-                        "author": self.get_coll_by_name(book, "author"),
-                        "publisher": self.get_coll_by_name(book, "publisher"),
-                        "quantity": self.get_coll_by_name(book, "quantity"),
-                        "rating": self.get_coll_by_name(book, "rating"),
-                    }
-                    books.append(book_cor)
+        if name not in rare_books:
+            self.common_books = self.common_books.drop_duplicates(subset=["name"])
+            self.common_books.reset_index(inplace=True)
+            self.common_books["index"] = [
+                i for i in range(self.common_books.shape[0])
+            ]
+            target_cols = ["name", "author", "publisher", "category"]
+            self.common_books["combined_features"] = [
+                " ".join(self.common_books[target_cols].iloc[i,].values)
+                for i in range(self.common_books[target_cols].shape[0])
+            ]
+            cv = CountVectorizer()
+            count_matrix = cv.fit_transform(self.common_books["combined_features"])
+            cosine_sim = cosine_similarity(count_matrix)
+            index = self.common_books[self.common_books["name"] == name][
+                "index"
+            ].values[0]
+            sim_books = list(enumerate(cosine_sim[index]))
+            self.sorted_sim_books = sorted(
+                sim_books, key=lambda x: x[1], reverse=True
+            )[1:size]
 
-                return books
+            books = []
+            for i in range(len(self.sorted_sim_books)):
+                book_cor = {
+                    "id": self.get_coll(i, "id"),
+                    "name": self.get_coll(i, "name"),
+                    "price": self.get_coll(i, "price"),
+                    "image": self.get_coll(i, "image"),
+                    "description": self.get_coll(i, "description"),
+                    "author": self.get_coll(i, "author"),
+                    "publisher": self.get_coll(i, "publisher"),
+                    "quantity": self.get_coll(i, "quantity"),
+                    "rating": self.get_coll(i, "rating"),
+                }
+                books.append(book_cor)
 
-            else:
-                self.common_books = self.common_books.drop_duplicates(subset=["name"])
-                self.common_books.reset_index(inplace=True)
-                self.common_books["index"] = [
-                    i for i in range(self.common_books.shape[0])
-                ]
-                target_cols = ["name", "author", "publisher", "category"]
-                self.common_books["combined_features"] = [
-                    " ".join(self.common_books[target_cols].iloc[i,].values)
-                    for i in range(self.common_books[target_cols].shape[0])
-                ]
-                cv = CountVectorizer()
-                count_matrix = cv.fit_transform(self.common_books["combined_features"])
-                cosine_sim = cosine_similarity(count_matrix)
-                index = self.common_books[self.common_books["name"] == name][
-                    "index"
-                ].values[0]
-                sim_books = list(enumerate(cosine_sim[index]))
-                self.sorted_sim_books = sorted(
-                    sim_books, key=lambda x: x[1], reverse=True
-                )[1:size]
+            return books
+        
+        logger.info("rare book")
+        random = pd.Series(self.common_books["name"].unique()).sample(16).values
+        books = []
+        for book in random:
+            book_cor = {
+                "id": self.get_coll_by_name(book, "id"),
+                "name": self.get_coll_by_name(book, "name"),
+                "price": self.get_coll_by_name(book, "price"),
+                "image": self.get_coll_by_name(book, "image"),
+                "description": self.get_coll_by_name(book, "description"),
+                "author": self.get_coll_by_name(book, "author"),
+                "publisher": self.get_coll_by_name(book, "publisher"),
+                "quantity": self.get_coll_by_name(book, "quantity"),
+                "rating": self.get_coll_by_name(book, "rating"),
+            }
+            books.append(book_cor)
 
-                books = []
-                for i in range(len(self.sorted_sim_books)):
-                    book_cor = {
-                        "id": self.get_coll(i, "id"),
-                        "name": self.get_coll(i, "name"),
-                        "price": self.get_coll(i, "price"),
-                        "image": self.get_coll(i, "image"),
-                        "description": self.get_coll(i, "description"),
-                        "author": self.get_coll(i, "author"),
-                        "publisher": self.get_coll(i, "publisher"),
-                        "quantity": self.get_coll(i, "quantity"),
-                        "rating": self.get_coll(i, "rating"),
-                    }
-                    books.append(book_cor)
-
-                return books
-        else:
-            return []
+        return books
